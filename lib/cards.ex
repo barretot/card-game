4f2d2a72f54faff7883cc17a7e8121bd31161777
barretot <barretoruan77@gmail.com>
2021-04-26 21:46:56 -0300

    fix

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,12 +1,27 @@
 defmodule Cards do
   # Criar deck
   def create_deck do
-    ["Ás", "Dois", "Três"]
+    values = ["Ás", "Dois", "Três", "Quatro", "Cinco"]
+
+    suits = ["Espadas", "Paus", "Copas", "Diamantes"]
+
+    for suit <- suits, value <- values do
+      "#{value} de #{suit}"
+    end
   end
 
   # Embaralhar
-  def shuffle do
-    create_deck()
-    |> Enum.shuffle()
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+    {:ok, card}
+  end
+
+  def deal(deck, hand_size) do
+    # Retornando uma tupla com os valores desejados
+    {hand, rest_of_deck} = Enum.split(deck, hand_size)
   end
 end
